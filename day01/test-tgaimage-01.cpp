01aeceae5869a194269075189cf2010e5e9dad46
Qunqun Xu <qunqun.xu@autodesk.com>
2024-01-28 13:14:59 +0800

    Fifth attempt, optimization

--- day01/test-tgaimage-01.cpp
+++ day01/test-tgaimage-01.cpp
@@ -43,7 +43,7 @@ void line2(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     }
 }
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line3(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     bool steep = false;
     if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
         std::swap(x0, y0);
@@ -73,6 +73,36 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     }
 }
 
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int derror2 = std::abs(dy) * 2;
+    int error2 = 0;
+    int y = y0;
+    for (int x = x0; x <= x1; x++) {
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
+        error2 += derror2;
+        if (error2 > dx) {
+            y += (y1 > y0 ? 1 : -1);
+            error2 -= dx * 2;
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     TGAImage image(100, 100, TGAImage::RGB);
 
@@ -81,6 +111,6 @@ int main(int argc, char **argv) {
     line(80, 40, 13, 20, image, red);
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("day01.003.tga");
+    image.write_tga_file("day01.004.tga");
     return 0;
 }

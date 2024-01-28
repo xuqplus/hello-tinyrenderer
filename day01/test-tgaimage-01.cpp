5ad70d71918cb8d1334b36c037b35cb47ba15dc5
Qunqun Xu <qunqun.xu@autodesk.com>
2024-01-28 12:47:21 +0800

    Third attempt

--- day01/test-tgaimage-01.cpp
+++ day01/test-tgaimage-01.cpp
@@ -12,12 +12,34 @@ void line0(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     }
 }
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line1(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     for (int x = x0; x <= x1; x++) {
         float t = (x - x0) / (float) (x1 - x0);
         int y = y0 * (1. - t) + y1 * t;
         image.set(x, y, color);
         std::cout << "x>" << x << ", y>" << y << ", " << std::endl;
+    }
+}
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1)) { // if the line is steep, we transpose the image
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1) { // make it left−to−right
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    for (int x = x0; x <= x1; x++) {
+        float t = (x - x0) / (float) (x1 - x0);
+        int y = y0 * (1. - t) + y1 * t;
+        if (steep) {
+            image.set(y, x, color); // if transposed, de−transpose
+        } else {
+            image.set(x, y, color);
+        }
     }
 }
 
@@ -29,6 +51,6 @@ int main(int argc, char **argv) {
     line(80, 40, 13, 20, image, red);
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("day01.001.tga");
+    image.write_tga_file("day01.002.tga");
     return 0;
 }
